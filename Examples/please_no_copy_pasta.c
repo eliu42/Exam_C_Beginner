b97ff65f955e2e324b234e3a42ddcd6a80ef8a54
Ezekiel Liu <liudezekiel@gmail.com>
2017-04-23 23:35:49 -0700

    no copy pasta please

---
+++ Examples/please_no_copy_pasta.c
@@ -0,0 +1,3 @@
+Please do not just copy and paste from my github or ANY github. It does not help your learn,
+and will only harm you in the future. If you have any questions on how something works, I
+Would be more than happy to explain it to you. Good luck on exams!
